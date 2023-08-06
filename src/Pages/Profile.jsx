9425f5608a3c1dd3b0767b169cf178a3a3e77f9b
SaesongMS <saesongms1@gmail.com>
2023-08-06 21:19:51 +0200

    Added spotify authorization for user profile

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,34 +1,50 @@
 import { useEffect, useState } from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import { redirectToAuthCodeFlow, getAccessToken } from "../Helpers/AuthPKCE";
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
 
-    const spotify_client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-    const [pkce_code, setPkceCode] = useState(undefined);
+    const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID
+    const REDIRECT_URI = "http://localhost:3000/profile"
+    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+    const RESPONSE_TYPE = "token"
+    const SCOPES = ["user-read-private", "user-read-email"]
 
-    const [profile, setProfile] = useState(null);
+    const loginURL = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPES.join("%20")}`
+
+    const [token, setToken] = useState("")
+    const [profile, setProfile] = useState(null)
 
     useEffect(() => {
-        const params = new URLSearchParams(window.location.search);
-        try {
-            setPkceCode(params.get('code'));
-        } catch (error) {
-            console.log(error);
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
+
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
         }
-        if(pkce_code === undefined){
-            redirectToAuthCodeFlow(spotify_client_id);
-        } else {
-            getProfile();
-        }
-    }, [spotify_client_id])
+
+        setToken(token)
+
+        if(token) getProfile();
+
+    }, [token])
+
+    const logout = () => {
+        setToken("")
+        window.localStorage.removeItem("token")
+        setProfile(null)
+    }
 
     const getProfile = async () => {
-        const spotify_access_token = await getAccessToken(pkce_code);
         const res = await fetch('https://api.spotify.com/v1/me', {
             method: 'GET',
             headers: {
-                Authorization: 'Bearer ' + spotify_access_token
+
+                Authorization: 'Bearer ' + token
             }
         });
         const data = await res.json();
@@ -40,7 +56,12 @@ function Profile() {
     <div>
         <Navbar />
         <h1>Profile</h1>
-        {profile && <h2>{profile.display_name}</h2>}
+        {!token ?
+            <a href={loginURL} onClick={console.log(loginURL)}>Login
+                to Spotify</a>
+            : <button onClick={logout}>Logout</button>}
+        {profile && <h2>{JSON.stringify(profile)}</h2>}
+
 
     </div>
     );
